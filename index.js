59ea6f2eee8ec87224271521d0b2d9cb6e014888
PindiVenkanna <155937857+PindiVenkanna@users.noreply.github.com>
2024-01-09 13:46:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber=Math.floor(Math.random()*100)+1;
+var attempts=0;
+
+document.getElementById("guessBtn").addEventListener("click",function(){
+    var guess =parseInt(document.getElementById("guessInput").value);
+    attempts++;
+    if(guess==randomNumber){
+        displaymsg("Congratulations! you Guessed the Number in "+attempts+" attempts.")
+        document.getElementById("guessBtn").disabled=true;
+    }
+    else if(guess<randomNumber){
+        displaymsg("Too Low Try another Number");
+    }
+    else{  
+        displaymsg("Too High Try another Number");
+    }
+
+});
+function displaymsg(k){
+    document.getElementById("msg").textContent=k;
+}
